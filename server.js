b708d1d19d9482bc7e859da5c07fb9ddcb01f0f4
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-03-26 14:09:25 +0200

    Add open to dev-server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import Koa from 'koa'
 import koaStatic from 'koa-static'
+import open from 'open'
 
 const server = new Koa()
 
 server.use(koaStatic('./www'))
 
 server.listen(3030)
+
+open('http://localhost:3030')
