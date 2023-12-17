89bd193e2195f4d6b3c37d1a90a49ecaf3148af7
VandeurenGlenn <vandeurenglenn@gmail.com>
2023-12-17 13:07:43 +0100

    Fix login & upgrade dependencies

--- src/screens/login.ts
+++ src/screens/login.ts
@@ -1,15 +1,14 @@
-import { html, css, LitElement } from "lit";
+import { html, css, LitElement } from 'lit'
 import generateAccount from '@leofcoin/generate-account'
 import IdentityController from '../controllers/identity.js'
 import '@material/web/button/elevated-button.js'
 import networks from '@leofcoin/networks'
-import QrScanner from "qr-scanner";
-import { decrypt, encrypt } from "@leofcoin/identity-utils";
+import QrScanner from 'qr-scanner'
+import { decrypt, encrypt } from '@leofcoin/identity-utils'
 import base58 from '@vandeurenglenn/base58'
 import type Client from '@leofcoin/endpoint-clients/direct'
-import { customElement, property, state } from "lit/decorators.js";
-import type Chain from "@leofcoin/chain/chain";
-
+import { customElement, property, state } from 'lit/decorators.js'
+import type Chain from '@leofcoin/chain/chain'
 
 declare global {
   var client: Client
@@ -17,17 +16,17 @@ declare global {
 }
 @customElement('login-screen')
 export class LoginScreen extends LitElement {
-  @property({type: Boolean, reflect: true})
+  @property({ type: Boolean, reflect: true })
   shown: boolean
   @state()
   mnemonic: string
-  @property({type: Boolean})
+  @property({ type: Boolean })
   hasWallet: boolean
   @state()
   importing: boolean
 
   async _hasWallet() {
-    const has = await globalThis.walletStorage?.has('identity')
+    const has = await globalThis.walletStore?.has('identity')
     return has
   }
 
@@ -35,7 +34,7 @@ export class LoginScreen extends LitElement {
     return this.renderRoot.querySelector('custom-pages')
   }
 
-  _keydown({key, code, keyCode, ctrlKey, altKey, shiftKey}) {
+  _keydown({ key, code, keyCode, ctrlKey, altKey, shiftKey }) {
     if (key === 'Enter') this.renderRoot.querySelector('[data-route-action="login"]').click()
   }
 
@@ -51,6 +50,8 @@ export class LoginScreen extends LitElement {
         const password = this.renderRoot.querySelector('input').value
         try {
           if (routeAction) {
+            console.log(routeAction)
+
             if (routeAction === 'create') await this.#handleCreate(password)
             if (routeAction === 'import') await this.#handleImport(password)
             if (routeAction === 'login') await this.#handleLogin(password)
@@ -80,41 +81,44 @@ export class LoginScreen extends LitElement {
   }
 
   async #handleBeforeLogin(password) {
+    console.log('handle')
 
-    
     this.renderRoot.querySelector('input').value = null
     let wallet
     this.hasWallet = await this._hasWallet()
     if (!this.hasWallet && !this.importing) {
       wallet = await generateAccount(password, 'leofcoin:peach')
-      globalThis.walletStorage.put('identity', JSON.stringify(wallet.identity))
-      globalThis.walletStorage.put('accounts', JSON.stringify(wallet.accounts))
-      globalThis.walletStorage.put('selectedAccount', wallet.accounts[0][1])
-      globalThis.walletStorage.put('selectedAccountIndex', '0')
+      globalThis.walletStore.put('identity', JSON.stringify(wallet.identity))
+      globalThis.walletStore.put('accounts', JSON.stringify(wallet.accounts))
+      globalThis.walletStore.put('selectedAccount', wallet.accounts[0][1])
+      globalThis.walletStore.put('selectedAccountIndex', '0')
     } else if (this.hasWallet) {
-      const identity = JSON.parse(await new TextDecoder().decode(await globalThis.walletStorage.get('identity')))
-      const accounts = JSON.parse(await new TextDecoder().decode(await globalThis.walletStorage.get('accounts')))
-      const selectedAccount = await new TextDecoder().decode(await globalThis.walletStorage.get('selectedAccount'))
-      const selectedAccountIndex = Number(await new TextDecoder().decode(await globalThis.walletStorage.get('selectedAccountIndex')))
-      wallet = {identity, accounts, selectedAccount, selectedAccountIndex}
+      const identity = JSON.parse(await new TextDecoder().decode(await globalThis.walletStore.get('identity')))
+      const accounts = JSON.parse(await new TextDecoder().decode(await globalThis.walletStore.get('accounts')))
+      const selectedAccount = await new TextDecoder().decode(await globalThis.walletStore.get('selectedAccount'))
+      const selectedAccountIndex = Number(
+        await new TextDecoder().decode(await globalThis.walletStore.get('selectedAccountIndex'))
+      )
+      wallet = { identity, accounts, selectedAccount, selectedAccountIndex }
     }
 
     globalThis.identityController = new IdentityController('leofcoin:peach', wallet)
+    console.log('handled')
+
     return wallet
   }
 
   async #handleAfterLogin(wallet) {
-
     document.querySelector('app-shell').wallet = wallet
-    
+
     if (!customElements.get('identity-view')) await import('../views/identity.js')
     const identityView = document.querySelector('app-shell').renderRoot.querySelector('identity-view')
     identityView.identity = wallet.identity
     identityView.accounts = wallet.accounts
     identityView.selectedAccount = wallet.selectedAccount
-    identityView.selectedAccountIndex = isNaN(Number(wallet.selectedAccountIndex)) ?
-      wallet.accounts.filter(([name, external, internal]) => external === wallet.selectedAccount)[0] :
-      wallet.selectedAccountIndex
+    identityView.selectedAccountIndex = isNaN(Number(wallet.selectedAccountIndex))
+      ? wallet.accounts.filter(([name, external, internal]) => external === wallet.selectedAccount)[0]
+      : wallet.selectedAccountIndex
 
     // this.hasWallet = await this._hasWallet()
     this.removeEventListener('keydown', this._keydown)
@@ -140,28 +144,28 @@ export class LoginScreen extends LitElement {
     try {
       await globalThis.identityController.unlock(password)
       await this.#handleAfterLogin(wallet)
-      
+
       this.loadChain(password)
     } catch (e) {
-      console.error(e);
+      console.error(e)
       throw e
     }
   }
 
-  #waitForKey = () => new Promise((resolve, reject) => {
-    const importSection = this.renderRoot.querySelector('[data-route="import"]')
+  #waitForKey = () =>
+    new Promise((resolve, reject) => {
+      const importSection = this.renderRoot.querySelector('[data-route="import"]')
 
-    const _onImport = (result) => {
-      
-      resolve(importSection.querySelector('input').value)
-      
-      importSection.querySelector('md-elevated-button').removeEventListener('click', _onImport)
-    }
-    importSection.querySelector('md-elevated-button').addEventListener('click', _onImport)
-    new QrScanner(this.renderRoot.querySelector('video'), (decoded) => {
-      importSection.querySelector('input').value = decoded
+      const _onImport = (result) => {
+        resolve(importSection.querySelector('input').value)
+
+        importSection.querySelector('md-elevated-button').removeEventListener('click', _onImport)
+      }
+      importSection.querySelector('md-elevated-button').addEventListener('click', _onImport)
+      new QrScanner(this.renderRoot.querySelector('video'), (decoded) => {
+        importSection.querySelector('input').value = decoded
+      })
     })
-  })
 
   async #handleImport(password) {
     this.importing = true
@@ -171,24 +175,24 @@ export class LoginScreen extends LitElement {
       const identityController = new IdentityController('leofcoin:peach')
 
       let wallet = await identityController.import(password, encrypted)
-      const multiWIF = new Uint8Array(await encrypt(password, await wallet.multiWIF));
+      const multiWIF = new Uint8Array(await encrypt(password, await wallet.multiWIF))
 
-      const external = await wallet.account(1).external(1);
-      const externalAddress = await external.address;
-      const internal = await wallet.account(1).internal(1);
-      const internalAddress = await internal.address;
+      const external = await wallet.account(1).external(1)
+      const externalAddress = await external.address
+      const internal = await wallet.account(1).internal(1)
+      const internalAddress = await internal.address
 
       wallet = {
         identity: {
-            multiWIF: base58.encode(multiWIF),
-            walletId: await external.id
+          multiWIF: base58.encode(multiWIF),
+          walletId: await external.id
         },
         accounts: [['main account', externalAddress, internalAddress]]
       }
-      globalThis.walletStorage.put('identity', JSON.stringify(wallet.identity))
-      globalThis.walletStorage.put('accounts', JSON.stringify(wallet.accounts))
-      globalThis.walletStorage.put('selectedAccount', wallet.accounts[0][1])
-      globalThis.walletStorage.put('selectedAccountIndex', '0')
+      globalThis.walletStore.put('identity', JSON.stringify(wallet.identity))
+      globalThis.walletStore.put('accounts', JSON.stringify(wallet.accounts))
+      globalThis.walletStore.put('selectedAccount', wallet.accounts[0][1])
+      globalThis.walletStore.put('selectedAccountIndex', '0')
     } catch (error) {
       console.error(error)
       alert(error)
@@ -198,21 +202,23 @@ export class LoginScreen extends LitElement {
   async #spawnChain(password) {
     let importee
     importee = await import('../../node_modules/@leofcoin/chain/exports/browser/node-browser.js')
-    await new importee.default({
-      network: 'leofcoin:peach',
-      networkName: 'leofcoin:peach',
-      networkVersion: 'peach',
-      stars: networks.leofcoin.peach.stars,
-      autoStart: false
-    },
-    password)
+    await new importee.default(
+      {
+        network: 'leofcoin:peach',
+        networkName: 'leofcoin:peach',
+        networkVersion: 'peach',
+        stars: networks.leofcoin.peach.stars,
+        autoStart: false
+      },
+      password
+    )
 
     importee = await import('@leofcoin/lib/node-config')
     const config = await importee.default()
 
     importee = await import('../../node_modules/@leofcoin/chain/exports/browser/chain.js')
-    globalThis.chain = await new importee.default()
-    console.log(chain);
+    globalThis.chain = await new importee.default({ resolveTimeout: 30_000 })
+    console.log(chain)
 
     this.#spawnEndpoint()
     // await globalThis.client.init()
@@ -227,9 +233,8 @@ export class LoginScreen extends LitElement {
       importee = await import('@leofcoin/endpoint-clients/ws')
       globalThis.client = await new importee.default('wss://ws-remote.leofcoin.org', 'peach')
       // @ts-ignore
-      globalThis.client.init && await globalThis.client.init()  
+      globalThis.client.init && (await globalThis.client.init())
     }
-    
   }
 
   async loadChain(password, direct = true) {
@@ -239,7 +244,7 @@ export class LoginScreen extends LitElement {
       if (direct) await this.#spawnChain(password)
       else await this.#spawnEndpoint(false)
     } catch (error) {
-      console.log(error);
+      console.log(error)
       this.#spawnEndpoint(false)
     }
   }
@@ -248,28 +253,26 @@ export class LoginScreen extends LitElement {
     const wallet = await this.#handleBeforeLogin(password)
 
     try {
-
       await globalThis.identityController.unlock(password)
-     
+
       await this.#handleAfterLogin(wallet)
       this.removeAttribute('shown')
       this.loadChain(password)
     } catch (e) {
-      console.error(e);
+      console.error(e)
       throw e
     }
   }
   get #defaultTemplate() {
-    return html`
-      <h4>Login</h4>
+    return html` <h4>Login</h4>
       <h5>Create a wallet or import one to continue</h5>
       <flex-it flex="2"></flex-it>
-      <input type="password" placeholder="password" tabindex="0" autofocus autocomplete="new-password">
+      <input type="password" placeholder="password" tabindex="0" autofocus autocomplete="new-password" />
       <flex-it></flex-it>
       <flex-row>
         <button data-route-action="import">import</button>
         <flex-it></flex-it>
-        <button data-route-action="create">create</button> 
+        <button data-route-action="create">create</button>
       </flex-row>`
   }
 
@@ -278,118 +281,116 @@ export class LoginScreen extends LitElement {
       <h4>Login</h4>
       <h5>Enter password to unlock wallet</h5>
       <flex-it flex="2"></flex-it>
-      <input type="password" placeholder="password" tabindex="0" autofocus autocomplete="current-password">
+      <input type="password" placeholder="password" tabindex="0" autofocus autocomplete="current-password" />
       <flex-it></flex-it>
       <button data-route-action="login" style="width: 100%; max-width: 190px; margin-bottom: 12px;">login</button>
-      `
+    `
   }
 
   static styles = css`
-  :host {
-    display: flex;
-    flex-direction: column;
-    inset: 0;
-    position: absolute;
-    align-items: center;
-    justify-content: center;
-    pointer-events: none;
-    opacity: 0;
-    background: #1116;
-  }
+    :host {
+      display: flex;
+      flex-direction: column;
+      inset: 0;
+      position: absolute;
+      align-items: center;
+      justify-content: center;
+      pointer-events: none;
+      opacity: 0;
+      background: #1116;
+    }
 
-  :host([shown]) {
-    opacity: 1;
-    pointer-events: auto;
-    z-index: 1002;
-  }
+    :host([shown]) {
+      opacity: 1;
+      pointer-events: auto;
+      z-index: 1002;
+    }
 
-  .wrapper {
-    background: var(--active-background);
-    border-radius: 12px;
-    box-sizing: border-box;
-    padding: 12px 24px;
-    height: 100%;
-    max-height: 240px;
-    max-width: 320px;
-    width: 100%;
-    color: var(--font-color);
-    border: 1px solid var(--border-color);
-    margin-left: 48px;
-  }
+    .wrapper {
+      background: var(--active-background);
+      border-radius: 12px;
+      box-sizing: border-box;
+      padding: 12px 24px;
+      height: 100%;
+      max-height: 240px;
+      max-width: 320px;
+      width: 100%;
+      color: var(--font-color);
+      border: 1px solid var(--border-color);
+      margin-left: 48px;
+    }
 
-  input, button {
-    border-color: white;
-    padding: 10px;
-    border-radius: 12px;
-    box-sizing: border-box;
-  }
+    input,
+    button {
+      border-color: white;
+      padding: 10px;
+      border-radius: 12px;
+      box-sizing: border-box;
+    }
 
-  input {
-    font-size: 16px;
-  }
+    input {
+      font-size: 16px;
+    }
 
-  button {
-    background: #12b8e4a3;
-    color: white;
-    border-color: white;
-    background: transparent;
-    padding: 10px 20px;
-  }
-  
-  h5 {
-    margin: 0;
-  }
+    button {
+      background: #12b8e4a3;
+      color: white;
+      border-color: white;
+      background: transparent;
+      padding: 10px 20px;
+    }
 
-  custom-pages {
-    width: 100%;
-    height: 100%;
-  }
+    h5 {
+      margin: 0;
+    }
 
-  span[data-route="login"] {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-  }
-  .word {
-    display: inline-flex;
-    padding: 6px;
-    border-radius: 3px;
-    background: #fff;
-    color: #333;
-  }
+    custom-pages {
+      width: 100%;
+      height: 100%;
+    }
+
+    span[data-route='login'] {
+      display: flex;
+      width: 100%;
+      height: 100%;
+      align-items: center;
+      justify-content: center;
+    }
+    .word {
+      display: inline-flex;
+      padding: 6px;
+      border-radius: 3px;
+      background: #fff;
+      color: #333;
+    }
   `
 
   render() {
     return html`
+      <flex-column class="wrapper">
+        <custom-pages attr-for-selected="data-route">
+          <flex-column data-route="login" center>
+            ${this.hasWallet ? this.#hasWalletTemplate : this.#defaultTemplate}
+          </flex-column>
 
-    <flex-column class="wrapper">   
-      <custom-pages attr-for-selected="data-route">
-        <flex-column data-route="login" center>
-        ${this.hasWallet ?  this.#hasWalletTemplate : this.#defaultTemplate}
-        </flex-column>
+          <flex-column data-route="create">
+            <h4>Make sure to backup your password and mnemonic</h4>
+            ${this.mnemonic}
 
-        <flex-column data-route="create">
-          <h4>Make sure to backup your password and mnemonic</h4>
-          ${this.mnemonic}
+            <md-elevated-button @click=${this.#iUnderstand}>I Understand</md-elevated-button>
+          </flex-column>
 
-          <md-elevated-button @click=${this.#iUnderstand}>I Understand</md-elevated-button>
-        </flex-column>
-
-        <flex-column data-route="import">
-            
+          <flex-column data-route="import">
             <flex-column data-route="qr">
               <video></video>
             </flex-column>
 
-            <input type="password" placeholder="multiwif" tabindex="0" autofocus autocomplete="new-password">
+            <input type="password" placeholder="multiwif" tabindex="0" autofocus autocomplete="new-password" />
 
             <md-elevated-button>import</md-elevated-button>
-          
-        </flex-column>
-      </custom-pages>
-    </flex-column>    
+          </flex-column>
+        </custom-pages>
+      </flex-column>
     `
   }
 }
--- src/views/wallet.ts
+++ src/views/wallet.ts
@@ -6,9 +6,8 @@ import { LitElement, PropertyValueMap, html } from 'lit'
 import { map } from 'lit/directives/map.js'
 import { customElement, property } from 'lit/decorators.js'
 import { consume } from '@lit-labs/context'
-import { walletContext, Address, Accounts, Wallet } from '../context/wallet.js';
+import { walletContext, Address, Accounts, Wallet } from '../context/wallet.js'
 import { CustomPages } from '@vandeurenglenn/custom-elements/pages.js'
-
 
 @customElement('wallet-view')
 export class WalletView extends LitElement {
@@ -35,7 +34,6 @@ export class WalletView extends LitElement {
   }
 
   protected willUpdate(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
-    
     if (_changedProperties.has('wallet')) {
       this.selectedAccount = this.wallet.selectedAccount
       this.accounts = this.wallet.accounts
@@ -56,22 +54,22 @@ export class WalletView extends LitElement {
     const to = this.#to.value
     const amount = this.#amount.value
     let from = this.selectedAccount
-    console.log({from});
-    const token = await client.nativeToken() as unknown as string
+    console.log({ from })
+    const token = (await client.nativeToken()) as unknown as string
 
-    const nonce = await client.getNonce(from) as number
+    const nonce = (await client.getNonce(from)) as number
     const rawTransaction = {
       timestamp: Date.now(),
       from,
-      to: token, 
+      to: token,
       method: 'transfer',
       nonce: nonce + 1,
       params: [from, to, parseUnits(amount).toString()]
     }
     const transaction = await signTransaction(rawTransaction, globalThis.identityController)
-    console.log(transaction);
+    console.log(transaction)
     const transactionEvent = await client.sendTransaction(transaction)
-    console.log(transactionEvent);
+    console.log(transactionEvent)
   }
 
   #handleClick = (event) => {
@@ -82,149 +80,156 @@ export class WalletView extends LitElement {
 
   render() {
     return html`
-<style>
-  * {
-    pointer-events: none;
-  }
+      <style>
+        * {
+          pointer-events: none;
+        }
 
-  .container flex-row {
-    width: 100%;
-  }
+        .container flex-row {
+          width: 100%;
+        }
 
-  :host {
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-  }
-  custom-pages {
-    width: 100%;
-    height: 100%;
-  }
-  [data-route="send"] {
+        :host {
+          display: flex;
+          flex-direction: row;
+          width: 100%;
+          height: 100%;
+          justify-content: center;
+        }
+        custom-pages {
+          width: 100%;
+          height: 100%;
+        }
+        [data-route='send'] {
+          width: 100%;
+          height: 100%;
+          align-items: center;
+          justify-content: center;
+        }
 
-    width: 100%;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-  }
+        .peer-id {
+          border: 1px solid white;
+          border-radius: 12px;
+          color: var(--font-color);
+          position: absolute;
+          /* top: 50%; */
+          left: 50%;
+          transform: translateX(-50%);
+          top: 12px;
+        }
 
-  .peer-id {
-    border: 1px solid white;
-    border-radius: 12px;
-    color: var(--font-color);
-    position: absolute;
-    /* top: 50%; */
-    left: 50%;
-    transform: translateX(-50%);
-    top: 12px;
-  }
-
-  /* .wallet-nav-container {
+        /* .wallet-nav-container {
     padding: 12px;
     box-sizing: border-box;
     height: 72px;
   } */
 
-  .wallet-nav {
-    border: 1px solid white;
-    background: var(--secondary-background);
-    border-radius: 12px 6px;
-    color: var(--font-color);
-    padding: 12px 6px;
-    box-sizing: border-box;
-    margin-bottom: 12px;
-  }
+        .wallet-nav {
+          border: 1px solid white;
+          background: var(--secondary-background);
+          border-radius: 12px 6px;
+          color: var(--font-color);
+          padding: 12px 6px;
+          box-sizing: border-box;
+          margin-bottom: 12px;
+        }
 
-  a {
-    padding: 0 12px;
-    cursor: pointer;
-  }
+        a {
+          padding: 0 12px;
+          cursor: pointer;
+        }
 
-  .container {
-    border-radius: 24px;
-    padding: 24px;
-    box-sizing: border-box;
-    background: var(--secondary-background);
-    color: var(--font-color);
-    border: 1px solid var(--border-color);
-    font-size: 18px;
-    width: 100%;
-    max-width: 320px;
-  }
+        .container {
+          border-radius: 24px;
+          padding: 24px;
+          box-sizing: border-box;
+          background: var(--secondary-background);
+          color: var(--font-color);
+          border: 1px solid var(--border-color);
+          font-size: 18px;
+          width: 100%;
+          max-width: 320px;
+        }
 
-  input {
-    margin-top: 12px;
-    margin-bottom: 24px;
-    box-sizing: border-box;
-    width: 100%;
-  }
+        input {
+          margin-top: 12px;
+          margin-bottom: 24px;
+          box-sizing: border-box;
+          width: 100%;
+        }
 
-  .main {
-    width: 100%;
-    align-items: center;
-  }
+        .main {
+          width: 100%;
+          align-items: center;
+        }
 
-  select, input, button {
-    pointer-events: auto;
-    background: transparent;
-    border: 1px solid var(--border-color);
-    font-size: 14px;
-    color: var(--font-color);
-    border-radius: 24px;
-    padding: 6px 12px;
-  }
+        select,
+        input,
+        button {
+          pointer-events: auto;
+          background: transparent;
+          border: 1px solid var(--border-color);
+          font-size: 14px;
+          color: var(--font-color);
+          border-radius: 24px;
+          padding: 6px 12px;
+        }
 
-  select, button {
-    cursor: pointer;
-  }
-</style>
+        select,
+        button {
+          cursor: pointer;
+        }
+      </style>
+      <custom-icon-set name="wallet-icons">
+        <template>
+          <span name="send">@symbol-input_circle</span>
+          <span name="receive">@symbol-output_circle</span>
+          <span name="list">@symbol-list</span>
+        </template>
+      </custom-icon-set>
 
-<flex-column class="main" @click=${this.#handleClick}>
-  <custom-pages attr-for-selected="data-route">
-    <flex-column data-route="send">
-      <flex-column class="container">
+      <flex-column class="main" @click=${this.#handleClick}>
+        <custom-pages attr-for-selected="data-route">
+          <flex-column data-route="send">
+            <flex-column class="container">
+              <flex-row>
+                <label for=".amount">send</label>
+                <flex-it></flex-it>
+                <select>
+                  <option>LFC</option>
+                </select>
+              </flex-row>
+              <input class="amount" placeholder="1" />
 
-        <flex-row>
-          <label for=".amount">send</label>
-          <flex-it></flex-it>
-          <select>
-            <option>LFC</option>
-          </select>
-        </flex-row>
-        <input class="amount" placeholder="1">
+              <label for=".to">to</label>
+              <input class="to" placeholder="address" />
 
-        <label for=".to">to</label>
-        <input class="to" placeholder="address">
+              <flex-it></flex-it>
+              <flex-row>
+                <button data-action="cancel">cancel</button>
+                <flex-it></flex-it>
+                <button data-action="send">send</button>
+              </flex-row>
+            </flex-column>
 
-        <flex-it></flex-it>
-        <flex-row>
-          <button data-action="cancel">cancel</button>
-          <flex-it></flex-it>
-          <button data-action="send">send</button>
+            <flex-column data-route="receive">
+              <clipboard-copy class="address peer-id" value=${this.selectedAccount}> </clipboard-copy>
+            </flex-column>
+          </flex-column>
+        </custom-pages>
+
+        <flex-row class="wallet-nav">
+          <a title="send">
+            <custom-icon set="wallet-icons" icon="send"></custom-icon>
+          </a>
+          <a title="recceive">
+            <custom-icon set="wallet-icons" icon="receive"></custom-icon>
+          </a>
+          <a title="transactions">
+            <custom-icon set="wallet-icons" icon="list"></custom-icon>
+          </a>
         </flex-row>
       </flex-column>
-
-      <flex-column data-route="receive">
-        <clipboard-copy class="address peer-id" value=${this.selectedAccount}>
-        </clipboard-copy>
-      </flex-column>
-    </flex-column>
-  </custom-pages>
-
-    <flex-row class="wallet-nav">
-      <a title="send">
-        <custom-svg-icon icon="send"></custom-svg-icon>
-      </a>
-      <a title="recceive">
-        <custom-svg-icon icon="receive"></custom-svg-icon>
-      </a>
-      <a title="transactions">
-        <custom-svg-icon icon="transactions"></custom-svg-icon>
-      </a>
-    </flex-row>
-</flex-column>
     `
   }
 }
